3e2bcd891e77c625f0daffa76bf8094d174e117c
mah <mahsa.79mirmiran@gmail.com>
2023-06-25 20:45:10 +0300

    add world .js and room.js

--- home3D/Experience/Experience.js
+++ home3D/Experience/Experience.js
@@ -3,7 +3,7 @@ import Sizes from "./Utils/Sizes"
 import Camera from "./Camera"
 import Renderer from "./Renderer"
 import Time from "./Utils/Time"
-
+import World from "./world/World"
 
 export default class Experience {
     static instance
@@ -18,6 +18,7 @@ export default class Experience {
         this.camera = new Camera();
         this.renderer = new Renderer();
         this.time = new Time();
+        this.world = new World();
 
         this.sizes.on("resize", () => {
             this.resize();
