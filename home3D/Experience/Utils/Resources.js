9a2f53461e101517e87ed78f4e6c26e48186a22b
mah <mahsa.79mirmiran@gmail.com>
2023-06-28 17:29:46 +0300

    room.js , resources.js, world.js

--- home3D/Experience/Utils/Resources.js
+++ home3D/Experience/Utils/Resources.js
@@ -2,6 +2,7 @@ import { EventEmitter } from "events";
 import Experience from "../Experience"
 import { GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
 import { DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader"
+import * as THREE from "three"
 
 export default class Resources extends EventEmitter {
     constructor(assets) {
@@ -28,17 +29,40 @@ export default class Resources extends EventEmitter {
         };
         this.loaders.gltfLoader = new GLTFLoader();
         this.loaders.dracoLoader = new DRACOLoader();
-        this.loaders.dracoLoader.setDecoderPath("/draco");
-        this.loaders.gltfLoader.setDecoderLoader(this.loaders.dracoLoader);
+        this.loaders.dracoLoader.setDecoderPath("/draco/");
+        this.loaders.gltfLoader.setDRACOLoader(this.loaders.dracoLoader);
     }
 
     startLoading(){
-        for(const assets of this.assets){
-            if (assets.type == "glbModel"){
-                this.loaders.gltfLoader .loade(asset.path,(file)=>{
+        for(const asset of this.assets){
+            if (asset.type == "glbModel"){
+                this.loaders.gltfLoader.load(asset.path,(file)=>{
                     this.singleAssetLoaded(asset, file);
                 });
+            } else if (asset.type === "videoTexture") {
+                this.video = {};
+                this.videoTexture = {};
+
+                this.video[asset.name] = document.createElement("video");
+                this.video[asset.name].src = asset.path;
+                this.video[asset.name].muted = true;
+                this.video[asset.name].playsInline = true;
+                this.video[asset.name].autoplay = true;
+                this.video[asset.name].loop = true;
+                this.video[asset.name].play();
+
+                this.videoTexture[asset.name] = new THREE.VideoTexture(
+                    this.video[asset.name]
+                );
+                // this.videoTexture[asset.name].flipY = false;
+                this.videoTexture[asset.name].minFilter = THREE.NearestFilter;
+                this.videoTexture[asset.name].magFilter = THREE.NearestFilter;
+                this.videoTexture[asset.name].generateMipmaps = false;
+                this.videoTexture[asset.name].encoding = THREE.sRGBEncoding;
+
+                this.singleAssetLoaded(asset, this.videoTexture[asset.name]);
             }
+
         }
     }
 
--- home3D/Experience/world/World.js
+++ home3D/Experience/world/World.js
@@ -9,6 +9,11 @@ export default class World {
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
         this.camera = this.experience.camera;
+        this.resources = this.experience.resources;
+
+        this.resources.on("ready",()=>{
+            this.room = new Room();
+        });
         this.room = new Room();
 
         
