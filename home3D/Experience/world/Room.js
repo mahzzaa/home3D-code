b6697cad09a6c66f342f14a7642656a00741dfa3
mah <mahsa.79mirmiran@gmail.com>
2023-06-28 17:29:56 +0300

    roomjs update

--- home3D/Experience/world/Room.js
+++ home3D/Experience/world/Room.js
@@ -5,12 +5,15 @@ export default class Room {
     constructor() {
         this.experience = new Experience();
         this.scene =this.experience.scene;
-        
         this.resources = this.experience.resources;
         this.room = this.resources.items.room;
         this.actualRoom = this.room.scene;
+        
+        this.setModel();
+    }
 
-        console.log(this.actualRoom);
+    setModel(){
+        this.scene.add(this.actualRoom);
     }
 
     resize() {
