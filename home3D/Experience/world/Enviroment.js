f4dc63558b4fb738abcb975c26d40b3de5ccdb91
mah <mahsa.79mirmiran@gmail.com>
2023-06-29 16:47:00 +0300

    add shadow+ edited aquwarium water

--- home3D/Experience/world/Enviroment.js
+++ home3D/Experience/world/Enviroment.js
@@ -14,10 +14,13 @@ export default class Enviroment {
         this.sunLight = new THREE.DirectionalLight("#ffffff" , 3);
         this.sunLight.castShadow = true;
         this.sunLight.shadow.camera.far =20;
-        this.sunLight.shadow.mapSize.set(1024,1024);
+        this.sunLight.shadow.mapSize.set(2048,2048);
         this.sunLight.shadow.normalBias = 0.05;
         this.sunLight.position.set (1.5, 7 ,3);
         this.scene.add(this.sunLight);
+
+        this.AmbientLight = new THREE.AmbientLight("#fffffff" , 1);
+        this.scene.add(this.AmbientLight);
     }
 
     resize() {
--- home3D/Experience/world/Room.js
+++ home3D/Experience/world/Room.js
@@ -4,18 +4,29 @@ import * as THREE from "three"
 export default class Room {
     constructor() {
         this.experience = new Experience();
-        this.scene =this.experience.scene;
+        this.scene = this.experience.scene;
         this.resources = this.experience.resources;
         this.room = this.resources.items.room;
         this.actualRoom = this.room.scene;
-        
+
         this.setModel();
     }
 
-    setModel(){
+    setModel() {
+        this.actualRoom.children.forEach(child => {
+            child.castShadow = true;
+            child.receiveShadow = true;
+
+
+            if (child instanceof THREE.Group) {
+                child.children.forEach((groupChild) => {
+                    groupChild.castShadow = true;
+                    groupChild.receiveShadow = true;
+                })
+            }
+        });
         this.scene.add(this.actualRoom);
-        this.actualRoom.scale.set(0.11,0.111,0.11);
-        this.actualRoom.rotation.y = Math.PI;
+        this.actualRoom.scale.set(0.11, 0.111, 0.11);
     }
 
     resize() {
