f823f355c8ed1d0684353f559e851e8848d17e04
mah <mahsa.79mirmiran@gmail.com>
2023-06-29 23:38:01 +0300

    edit camera shadow

--- home3D/Experience/world/Enviroment.js
+++ home3D/Experience/world/Enviroment.js
@@ -4,22 +4,26 @@ import * as THREE from "three"
 export default class Enviroment {
     constructor() {
         this.experience = new Experience();
-        this.scene =this.experience.scene;
+        this.scene = this.experience.scene;
         this.resources = this.experience.resources;
-        
+
         this.setSunlight();
     }
 
-    setSunlight(){
-        this.sunLight = new THREE.DirectionalLight("#ffffff" , 3);
+    setSunlight() {
+        this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
         this.sunLight.castShadow = true;
         this.sunLight.shadow.camera.far = 2;
-        this.sunLight.shadow.mapSize.set(2048,2048);
-        this.sunLight.shadow.normalBias = 0.05;
-        this.sunLight.position.set (2, 7 ,3);
+        this.sunLight.shadow.mapSize.set(2048, 2048);
+        this.sunLight.shadow.normalBias = 1;
+
+         const helper = new THREE.CameraHelper(this.sunLight.shadow.camera);
+         this.scene.add(helper);
+
+        this.sunLight.position.set(0, 7, 3);
         this.scene.add(this.sunLight);
 
-        this.AmbientLight = new THREE.AmbientLight("#ffffff" , 2);
+        this.AmbientLight = new THREE.AmbientLight("#ffffff", 2);
         this.scene.add(this.AmbientLight);
     }
 
