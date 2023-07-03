ad98c6c5766070830d8fb1dbe26e16c1e12a087a
mah <mahsa.79mirmiran@gmail.com>
2023-07-04 01:13:38 +0300

    add shadow and some update on floor.js and camera.js

--- home3D/Experience/world/Floor.js
+++ home3D/Experience/world/Floor.js
@@ -15,10 +15,15 @@ export default class Floor {
         this.geometry = new THREE.PlaneGeometry(100,100);
         this.material = new THREE.MeshStandardMaterial({
             color: 0xffffff,
-
+            side: THREE.BackSide,
         });
         this.plane = new THREE.Mesh(this.geometry , this.material);
         this.scene.add(this.plane);
+        this.plane.rotation.x = Math.PI /2;
+        this.plane.rotation.y = -0.3;
+        this.plane.receiveShadow = true;
+        
+
     }
     resize() {
 
