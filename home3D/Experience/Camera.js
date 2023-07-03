7c9b024976b600ac46e38649c564ba51bf7111c3
mah <mahsa.79mirmiran@gmail.com>
2023-07-04 00:55:58 +0300

    update room.js

--- home3D/Experience/Camera.js
+++ home3D/Experience/Camera.js
@@ -45,27 +45,19 @@ export default class Camera {
         this.orthographicCamera.position.z = 5;
         this.orthographicCamera.rotation.x = -Math.PI/6;
 
-
-        //   this.orthographicCamera = new THREE.PerspectiveCamera(
-        //     35,
-        //     this.sizes.aspect,
-        //     0.1,
-        //     1000
-        // );
-
         this.scene.add(this.orthographicCamera);
 
-        this.helper = new THREE.CameraHelper(this.orthographicCamera);
-        this.scene.add(this.helper);
+        // this.helper = new THREE.CameraHelper(this.orthographicCamera);
+        // this.scene.add(this.helper);
 
         const size = 20;
         const divisions = 20;
 
-        const gridHelper = new THREE.GridHelper(size, divisions);
-        this.scene.add(gridHelper);
+        // const gridHelper = new THREE.GridHelper(size, divisions);
+        // this.scene.add(gridHelper);
 
-        const axesHelper = new THREE.AxesHelper(10);
-        this.scene.add(axesHelper);
+        // const axesHelper = new THREE.AxesHelper(10);
+        // this.scene.add(axesHelper);
     }
 
     setOrbitControls() {
@@ -90,9 +82,10 @@ export default class Camera {
 
     update() {
         this.controls.update();
-        this.helper.matrixWorldNeedsUpdate = true;
-        this.helper.update();
-        this.helper.position.copy(this.orthographicCamera.position);
-        this.helper.rotation.copy(this.orthographicCamera.rotation);
+
+        // this.helper.matrixWorldNeedsUpdate = true;
+        // this.helper.update();
+        // this.helper.position.copy(this.orthographicCamera.position);
+        // this.helper.rotation.copy(this.orthographicCamera.rotation);
     }
 }
